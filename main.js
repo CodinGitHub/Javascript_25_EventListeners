4bd9790396677ac528a6bada4e7fd7a4106f0446
CodinGitHub <gms2programador@gmail.com>
2022-03-31 06:08:16 -0500

    ejemplo de submits

---
+++ main.js
@@ -0,0 +1,11 @@
+let submitBtn = document.querySelector('input[type="text"]');
+submitBtn.addEventListener('click', ()=>{
+    console.log('Se ha realizado un click');
+});
+
+// function saludo(event){
+//     console.log(event.target.innerHTML);
+// }
+
+// saludoBtn.addEventListener('click', saludo);
+// saludoBtn.removeEventListener('click', saludo);
